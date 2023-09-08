95d5e1da370b52a77fa8eea6e16b98839452478e
Florzi <106540640+Florzi@users.noreply.github.com>
2023-09-08 10:57:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 alert('Olá, mundo');
+<script src="main.js"></script>
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+<button onclick="tocaSomPom()” class="tecla "tecla_pom”>Pom</button>
+document.querySelector('.tecla_pom').onclick> ;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+    }
+    document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    document.querySelectorAll('.tecla');
+
+
+
